2b9ca1c5cc0e03b5301b10396e17b2575e49a675
codewiz <c.chaiwat.ch@gmail.com>
2020-04-01 13:12:39 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkFrom from './components/ImageLinkFrom/ImageLinkFrom'
+import Rank from './components/Rank/Rank'
+import Clarifai from 'clarifai'
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkFrom />
-      {/*
-      <FaceRecognition />
-      */}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: 'e80301fd2e9743fe9576acb4a154a234'
+})
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+    app.models
+    .predict(
+    Clarifai.COLOR_MODEL,
+        // URL
+        "https://samples.clarifai.com/metro-north.jpg"
+    )
+    .then(function(response) {
+        // do something with responseconsole.log(response);
+    }, function(err) {// there was an error}
+
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className="particles"
+          params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkFrom
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
